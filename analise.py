7ec542e3b8d08e8c1e6eb40a540f8427e50bb463
Larissa Guder <lariguder10@gmail.com>
2022-03-21 22:03:10 -0300

    feat: calculo de moda

--- analise.py
+++ analise.py
@@ -1,7 +1,14 @@
 import numpy as np
+from scipy.stats import mode
 
-## >>>> Exemplo de cálculo de frequência
-banda = np.array(['Fresno', 'Cuscobayo', 'Fresno', 'Fresno', 'Cuscobayo', 'Fresno', 'Fresno', 'Maglore' ])
+# >>>> Exemplo de cálculo de frequência
+banda = np.array(['Fresno', 'Cuscobayo', 'Fresno', 'Fresno',
+                 'Cuscobayo', 'Fresno', 'Fresno', 'Maglore'])
 # Calcular a frequencia da categoria banda no atributo Fresno
 frequencia = (banda == 'Fresno').sum() / float(banda.size)
-print ('Frequência da categoria "Fresno" é ', frequencia)
+print('Frequência da categoria "Fresno" é ', frequencia)
+
+# Calcular moda
+moda, quantidade = mode(banda)
+print('A moda é a categoria "{}". Quantidade de repetições: {}'.format(
+    moda[0], quantidade))
